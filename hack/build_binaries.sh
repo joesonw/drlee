5080dbbdc5225161bf3b73396e4e9fcb734b1532
Qiaosen Huang <joesonw@gmail.com>
2020-07-07 10:08:55 +0800

    fix build script under linux

--- hack/build_binaries.sh
+++ hack/build_binaries.sh
@@ -10,6 +10,14 @@ ALL_ARCH=(amd64)
 BAZEL=${BAZEL_BIN:=bazel}
 HOSTOS="$(go env GOHOSTOS)"
 
+if [[ $HOSTOS == "linux" ]] then
+  HOSTOS="k8"
+fi
+
+if [[ $HOSTOS == "windows" ]] then
+  HOSTOS="x64_windows"
+fi
+
 for OS in ${ALL_OS[@]}; do
     EXT=""
     if [[ $OS == "windows" ]]; then
@@ -18,7 +26,7 @@ for OS in ${ALL_OS[@]}; do
     for ARCH in ${ALL_ARCH[@]}; do
           FILE="${ROOT}/binaries/drlee-${OS}-${ARCH}${EXT}"
           ${BAZEL} build --platforms=@io_bazel_rules_go//go/toolchain:${OS}_${ARCH} //:drlee
-          cp ${ROOT}/bazel-out/${HOSTOS}-fastbuild/bin/drlee_/drlee${EXT} ${FILE}
+          cp ${ROOT}/bazel-out/${$HOSTOS}-fastbuild/bin/drlee_/drlee${EXT} ${FILE}
     done
 done
 
